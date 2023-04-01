1ef2fcf00c0d6ea6ce2704be8d8e4c4b41311385
Yashwanth <mvyashwanthkumar@gmail.com>
2023-04-02 00:51:53 +0530

    Quick Select Approach

--- Arrays/kth_largest.cpp
+++ Arrays/kth_largest.cpp
@@ -37,9 +37,38 @@ int min_heap(vector<int> &nums,int k){
     return min_heap.top();
 }
 
+    /*int efficient(vector<int>& nums, int k) {
+        return quick_select(0, nums.size() - 1, nums, k);
+    }*/
+
+    int quick_select(int l, int r, vector<int>& nums, int k) {
+        /*int rand_pivot = l + rand() % (r - l + 1);
+        int pivot = nums[rand_pivot];
+        int p = l;
+        swap(nums[rand_pivot], nums[r]);*/
+        int p=l;
+        int pivot=nums[r];
+        for (int i = l; i < r; i++) {
+            if (nums[i] >= pivot) {
+                swap(nums[p], nums[i]);
+                p++;
+            }
+        }
+        swap(nums[p], nums[r]);
+
+        if (p == k - 1) {
+            return nums[p];
+        } else if (p > k - 1) {
+            return quick_select(l, p - 1, nums, k);
+        } else {
+            return quick_select(p + 1, r, nums, k);
+        }
+    }
+
 int main(){
     vector<int>arr={1,2,3,4};
     cout<<sort(arr,2)<<endl;
     cout<<max_heap(arr,2)<<endl;
     cout<<min_heap(arr,2)<<endl;
+    cout<<quick_select(0,arr.size()-1,arr,2)<<endl;
 }
