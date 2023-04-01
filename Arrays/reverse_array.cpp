fbe857046cf4bfdd852bcb254ec5a57fb01046bf
Yashwanth <mvyashwanthkumar@gmail.com>
2023-04-01 16:13:36 +0530

    Added recursive

--- Arrays/reverse_array.cpp
+++ Arrays/reverse_array.cpp
@@ -3,7 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void reverse(int arr[],int n){
+/*void reverse(int arr[],int n){
     int i=0;
     while(i<n){
         int temp=arr[i];
@@ -21,4 +21,26 @@ int  main(){
     for(int i=0;i<n;i++)
         cout<<arr[i]<<" ";
     return 0;
+}*/
+
+
+//Recursive
+void reverse(int arr[],int start,int end){
+    if(start>=end)
+        return;
+    int temp=arr[start];
+    arr[start]=arr[end];
+    arr[end]=temp;
+
+    reverse(arr,start+1,end-1);
 }
+
+int  main(){
+    int arr[]={1,2,3,4,5,6};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    reverse(arr,0,n-1);
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+    return 0;
+}
+
