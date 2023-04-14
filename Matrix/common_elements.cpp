7c295f6204f74ae070adb4aab835eb5b9e1a6c69
Yashwanth <mvyashwanthkumar@gmail.com>
2023-04-14 19:35:26 +0530

    Hashmap

---
+++ Matrix/common_elements.cpp
@@ -0,0 +1,59 @@
+/*Given an m x n matrix, find all common elements present in all rows in O(mn) time and one traversal
+ of matrix.*/
+
+/*We have to use hashmap in order to track elements if count =number of rows and row=i-1 we return 
+the element.*/
+// A Program to prints common element in all
+// rows of matrix
+#include <bits/stdc++.h>
+using namespace std;
+
+// Specify number of rows and columns
+#define M 4
+#define N 5
+
+// prints common element in all rows of matrix
+void printCommonElements(int mat[M][N])
+{
+	unordered_map<int, int> mp;
+
+	// initialize 1st row elements with value 1
+	for (int j = 0; j < N; j++)
+		mp[(mat[0][j])] = 1;
+
+	// traverse the matrix
+	for (int i = 1; i < M; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			// If element is present in the map and
+			// is not duplicated in current row.
+			if (mp[(mat[i][j])] == i)
+			{
+			// we increment count of the element
+			// in map by 1
+				mp[(mat[i][j])] = i + 1;
+
+				// If this is last row
+				if (i==M-1 && mp[(mat[i][j])]==M)
+				cout << mat[i][j] << " ";
+			}
+		}
+	}
+}
+
+// driver program to test above function
+int main()
+{
+	int mat[M][N] =
+	{
+		{1, 2, 1, 4, 8},
+		{3, 7, 8, 5, 1},
+		{8, 7, 7, 3, 1},
+		{8, 1, 2, 7, 9},
+	};
+
+	printCommonElements(mat);
+
+	return 0;
+}
